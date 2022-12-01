d37e6f98ea63c05ba8817fe25d6e4df92f2e0ce7
Muzaffar <mus.ariff@gmail.com>
2022-12-01 10:17:30 +0800

    registrations and login done

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,14 +6,20 @@ class RegistrationsController < ApplicationController
   def create
     @new_user = User.new(registration_params)
     
-    
+    if @new_user.save
+      flash[:notice] = "User successfully created, please login"
+      redirect_to new_login_path 
+    else
+      flash[:alert] = "Email and/or Password could not be created!"
+      render  :new, status: :unprocessable_entity
+    end
     
   end
 
   private
   
   def registration_params
-    params.require(:registration).permit(:email, :password)
+    params.require(:user).permit(:email, :password)
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   
-  root "registrations#new"
+  root "logins#new"
   
   resources :registrations, only: %i[new create]
-  
+  resources :logins,        only: %i[new create]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
